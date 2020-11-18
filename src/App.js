5c2c33462ac907a795eaa4be0d92f682fb6c9c10
Craig McLeod <mcleod.craig@outlook.com>
2020-11-18 12:17:30 +0000

    Firebase google authentication set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import firebase from "firebase/app";
+
+import withFirebaseAuth from 'react-with-firebase-auth'
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig.js';
+import UserHome from './userHome';
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+function App(props) {
+
+  const {
+    user, 
+    signOut, 
+    signInWithGoogle,
+  } = props;
+
+  console.log(signInWithGoogle, user)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      {
+        user 
+          ? <UserHome user={user} />
+          : <p>Please sign in.</p>
+      }
+      {
+        user
+          ? <button onClick={signOut}>Sign out</button>
+          : <button onClick={signInWithGoogle}>Sign in with Google</button>
+      }
+    </header>
+  </div>
   );
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/userHome.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react'
+import React, {useState, useEffect} from 'react'
+
+const UserHome = () =>{
+
+    return(
+
+        <h1> Welcome to the quiz generatorrrrr, quizzards! </h1>
+
+    )
+
+}
+
+export default UserHome;
